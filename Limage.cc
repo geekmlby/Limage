12785947856dc6042a834d19e3afc5c03be24655
geekmlby <geekmlby@163.com>
2018-09-05 22:50:17 +0800

    Limage has a lib!

--- Limage.cc
+++ Limage.cc
@@ -8,9 +8,9 @@ using namespace std;
 int main(int argc,char* argv[])
 {
 	CDIP edgeDete;
-    char path[PATHSIZE] = "/home/wangli/Limage/img/lena.bmp";
+    //char path[PATHSIZE] = "/home/wangli/Limage/img/lena.bmp";
     //char path[PATHSIZE] = "/home/wangli/Limage/img/lena_salt.bmp";
-	//char path[PATHSIZE] = "/home/wangli/Limage/img/chessboard.jpg";
+	char path[PATHSIZE] = "/home/wangli/Limage/img/chessboard.jpg";
     /*cout << "Please input path:" << endl;     //注：手动输入图像路径。
     cin.get(path,100);*/   
 	edgeDete.ReadImage(path);
@@ -19,24 +19,35 @@ int main(int argc,char* argv[])
 	uchar* puc_Mat = new uchar[MAXHEIGHT * MAXWIDTH]; 
 	uchar* puc_Mat2 = new uchar[MAXHEIGHT * MAXWIDTH];    
 	edgeDete.GetGrayImage(puc_Mat);
-	edgeDete.ShowImage("GrayImage",
+	/*edgeDete.ShowImage("GrayImage",
+					   puc_Mat,
+					   edgeDete.imgH,
+					   edgeDete.imgW);*/
+
+	/*edgeDete.GausBlur(puc_Mat,
+					  edgeDete.imgH,
+					  edgeDete.imgW,
+					  5,
+					  5,
+					  1.5);
+	edgeDete.ShowImage("GausImage",
+					   puc_Mat,
+					   edgeDete.imgH,
+					   edgeDete.imgW);*/
+
+	edgeDete.EdgeDete_Canny(puc_Mat,
+						  	edgeDete.imgH,
+						  	edgeDete.imgW,
+						  	0.2,
+							0.1);
+	edgeDete.ShowImage("Canny",
 					   puc_Mat,
 					   edgeDete.imgH,
 					   edgeDete.imgW);
-
-	edgeDete.Stretch(puc_Mat2,
-					 puc_Mat,
-					 edgeDete.imgH,
-					 edgeDete.imgW,
-					 30);
-	edgeDete.ShowImage("Stretch",
+	/*edgeDete.SaveImage("/home/wangli/Limage/BilinearInte.bmp",
 					   puc_Mat2,
-					   edgeDete.imgH,
-					   edgeDete.imgW);
-	edgeDete.SaveImage("/home/wangli/Limage/Stretch.bmp",
-					   puc_Mat2,
-					   edgeDete.imgH,
-					   edgeDete.imgW);
+					   edgeDete.imgH * 2,
+					   edgeDete.imgW * 2);*/
 	delete puc_Mat;
 	delete puc_Mat2;
 	puc_Mat = NULL;
